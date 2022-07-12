117f39c1aee1b20d7873b3b559b9d33f40688c87
Hamart Rony <hamart23@icloud.com>
2022-07-12 10:13:19 -0300

    update: model and relations

--- games/models.py
+++ games/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name: models.StringField(max_length=64, unique=True)
+    image: models.StringField()
+    release_date: models.DateField()
+
+
+rent_accounts = models.ManyToManyField("rent_accounts", related_name="rent_accounts")
