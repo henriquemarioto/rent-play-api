8df3d2d5b96ed7996544bee96fc98ce3a6b911ad
jeanknieling <jeanknieling@gmail.com>
2022-07-12 16:23:16 -0300

    nothing

--- rent_accounts/models.py
+++ rent_accounts/models.py
@@ -1,20 +1,3 @@
-from tkinter import CASCADE
 from django.db import models
-import uuid
 
-
-class RentAccount(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    plataform = models.CharField(max_length=16)
-    login = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    price_per_day = models.DecimalField(max_digits=6, decimal_places=2)
-
-    renter = models.OneToOneField(
-        "users.User", on_delete=CASCADE, related_name="renter"
-    )
-    owner = models.OneToOneField("users.User", on_delete=CASCADE, related_name="owner")
-
-    games = models.ManyToManyField(
-        "games.Game", related_name="rent_accounts", on_delete=models.CASCADE
-    )
+# Create your models here.
