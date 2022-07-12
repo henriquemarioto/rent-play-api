3dbadd5e317ac6ee2119ab4e7b5731e2fa2839ed
Hamart Rony <hamart23@icloud.com>
2022-07-12 12:28:49 -0300

    update : uuid in models

--- games/models.py
+++ games/models.py
@@ -1,11 +1,9 @@
 from django.db import models
+import uuid
 
 
 class Game(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name: models.StringField(max_length=64, unique=True)
     image: models.StringField()
     release_date: models.DateField()
-
-    rent_accounts = models.ManyToManyField(
-        "rent_accounts", related_name="rent_accounts", on_delete=models.CASCADE
-    )
--- rents_history/models.py
+++ rents_history/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 
 class RentHistory(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     start_date = models.DateField()
     end_date = models.DateField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -9,7 +11,7 @@ class RentHistory(models.Model):
 
     rent_accounts = models.ManyToManyField(
         "rent_accounts.RentAccount",
-        related_name="rent_accounts",
+        related_name="rent_account",
         on_delete=models.DO_NOTHING,
     )
     user = models.ManyToManyField(
