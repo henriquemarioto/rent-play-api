4844ad75ec110129497b337cbb74dd57c37efcb9
Guilherme Preveda Milék <guilhermeprevedamilek@gmail.com>
2022-07-13 00:04:39 -0300

    fix: add url validation in game model

--- rents_history/models.py
+++ rents_history/models.py
@@ -5,14 +5,12 @@ import uuid
 
 class RentHistory(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-<<<<<<< HEAD
     start_date = models.DateField(default=datetime.date.today)
-=======
-    start_date = models.DateField()
->>>>>>> a69e6231de8739a433716e0f9ef9d7cf990f030c
     end_date = models.DateField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     reseted = models.BooleanField(default=False)
 
-    rent_accounts = models.ManyToManyField("rent_accounts.RentAccount", related_name="rent_accounts")
+    rent_accounts = models.ManyToManyField(
+        "rent_accounts.RentAccount", related_name="rent_accounts"
+    )
     user = models.ManyToManyField("users.User", related_name="user")
