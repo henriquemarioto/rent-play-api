2bad1aef194064e7fa4c16f3fc8fd5cda29211c9
henriquemarioto <henrique.marioto@outlook.com>
2022-07-13 00:10:55 -0300

    fix: rent history model start date

--- rents_history/models.py
+++ rents_history/models.py
@@ -5,11 +5,7 @@ import uuid
 
 class RentHistory(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-<<<<<<< HEAD
     start_date = models.DateField(default=datetime.date.today)
-=======
-    start_date = models.DateField()
->>>>>>> a69e6231de8739a433716e0f9ef9d7cf990f030c
     end_date = models.DateField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     reseted = models.BooleanField(default=False)
