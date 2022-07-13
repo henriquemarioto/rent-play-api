091eaec21ab8e453c2ab3b9a569eb6d303522c3e
henriquemarioto <henrique.marioto@outlook.com>
2022-07-13 00:07:41 -0300

    fix: cors

--- rent_play/settings.py
+++ rent_play/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# CORS_ORIGIN_ALLOW_ALL=True
+#  CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_ALL_ORIGINS=True
 
 ROOT_URLCONF = "rent_play.urls"
