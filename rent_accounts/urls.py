fdfaa2bf452b001b87ae9d54d6505fe541105dcd
Hamart Rony <hamart23@icloud.com>
2022-07-15 01:03:02 -0300

    feat: created url rent_account by owner

--- rent_accounts/urls.py
+++ rent_accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ListCreateRentAccountView, RetrieveUpdateDestroyRentAccountView, AddGamesRentAccountByIdView, RemoveGamesRentAccountByIdView, UpdateRentRentAccountByIdView
+from .views import ListCreateRentAccountView, RetrieveUpdateDestroyRentAccountView, AddGamesRentAccountByIdView, RemoveGamesRentAccountByIdView, UpdateRentRentAccountByIdView, ListRentAccountOwnerView
 
 urlpatterns = [
     path("rent_accounts/", ListCreateRentAccountView.as_view()),
+    path("rent_accounts/<pk>/owner/", ListRentAccountOwnerView.as_view()),
     path("rent_accounts/<pk>/", RetrieveUpdateDestroyRentAccountView.as_view()),
     path("rent_accounts/<pk>/games/add/", AddGamesRentAccountByIdView.as_view()),
     path("rent_accounts/<pk>/games/remove/", RemoveGamesRentAccountByIdView.as_view()),
