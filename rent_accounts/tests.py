838fdc39b5fe361deaabafca109826facaf04d9c
Hamart Rony <hamart23@icloud.com>
2022-07-20 14:37:59 -0300

    update : test wallet, superuser, rent_history

--- rent_accounts/tests.py
+++ rent_accounts/tests.py
@@ -12,29 +12,46 @@ from platforms.models import Platform
 from rest_framework.views import status
 
 class RentAccountModelTest(APITestCase):
-     
-    # CRIAR VENDEDOR
+
     @classmethod    
     def setUpTestData(cls) -> None:
+        #CRIAR SUPER USER
+        cls.super_user = {
+            "nickname": "Admin",
+            "first_name": "Admin",
+            "last_name":  "Admin",
+            "password": "12345678",
+            "cellphone": "55999999991",
+            "email": "admin@rentandplay.com.br"
+        }
+        cls.admin = User.objects.create_superuser(**cls.super_user) 
+
+        #CRIAR USER 1
+        cls.tester1_data = {
+            "nickname" : "Tester1",
+            "first_name" : "Tester1",
+            "last_name" : "Anonimous",
+            "cellphone" : "12345678916",
+            "email" : "tester1@email.com",
+            "password" : "123456",
+        }
+
+        cls.tester1 = User.objects.create_user(**cls.tester1_data) 
+
+        #CRIAR USER 2
         cls.tester2_data = {
             "nickname" : "Tester2",
-            "first_name" : "Tester",
+            "first_name" : "Tester2",
             "last_name" : "Anonimous",
             "cellphone" : "12345678915",
             "email" : "tester2@email.com",
             "password" : "123456"
         }
-        
+
         cls.tester2 = User.objects.create_user(**cls.tester2_data)
-    # BUSCAR PLATAFORMA 
-        # cls.platfrom_data = {
-        #     "platform_api_id" : "1",
-        #     "icon" : "testeicon",
-        #     "name" : "xbox",
-        #     "image_url" : "https://files.tecnoblog.net/meiobit/wp-content/uploads/2019/11/20191122god-of-war.jpg"
-        # }   
+
        
-        
+        # BUSCAR PLATAFORMAS 
         cls.platform1 = Platform.objects.get(platform_api_id=1)
         cls.platform2 = Platform.objects.get(platform_api_id=2)
         cls.platforms = [cls.platform1, cls.platform2]
@@ -63,11 +80,9 @@ class RentAccountModelTest(APITestCase):
         }
         cls.rent_account = RentAccount.objects.create(**cls.rent_account_data)   
 
-    def test_attr_(self):
-        print("PLATFORMMMMMMMMMM", self.platform1.__dict__)
-        # print("AQUIIIIII PLATFOMMMMMMMMMMMMMM", self.platforms[0].__dict__)                    
+    #TESTE ATRIBUTOS MODEL RENT_ACCOUNT
+    def test_attr_(self):         
         for item in self.platforms:
-            # print("ITEMMMMMMMMMMM", item)
             self.games[0].platforms.add(item)
         for item in self.games:
             self.rent_account.games.add(item)        
@@ -78,11 +93,17 @@ class RentAccountModelTest(APITestCase):
         self.assertEquals(self.rent_account.owner, self.rent_account_data["owner"])
         self.assertEquals(len(self.games), self.rent_account.games.count())       
 
-# class RentAccountViewTest(APITestCase):    
+    
+    # LOGIN USERS
     def test_login_user(self):       
         
         res = self.client.post("/login/", data={"email": self.tester2_data.get("email"), "password": self.tester2_data.get("password")})
-        token = Token.objects.get(user=self.tester2)             
+        token = Token.objects.get(user=self.tester2) 
+
+        res1 = self.client.post("/login/", data={"email": self.tester1_data.get("email"), "password": self.tester1_data.get("password")})
+        token1 = Token.objects.get(user=self.tester1) 
+
+
        
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(token.key, res.data["token"])
@@ -128,17 +149,29 @@ class RentAccountModelTest(APITestCase):
 
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)       
-        
+
+    def test_update_user_wallet(self):
+        self.client.force_authenticate(user=self.tester1)
+        wallet = {"wallet":500.00}
+        response = self.client.patch(f"/users/{self.tester1.id}/", data=wallet, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  
+
+
+
        
     def test_create_rents_history(self):
-        self.client.force_authenticate(user=self.tester2)
+        account = self.rent_account.id
+        print("RENTTTTTTTT", account)
+        print("RENTTTTTTTT", self.tester1.__dict__)
+        self.client.force_authenticate(user=self.tester1)
         rents_days = {
-            "rented_days": "7"
+            "rented_days": 7
         }
         
 
-        response = self.client.patch("/rent_accounts/<pk>/rent/", data=rents_days, format='json')       
+        response = self.client.patch(f"/rent_accounts/{account}/rent/", data=rents_days, format='json')  
+        print("RESPONSEEEEEEEEEEEEEE", response.data)     
         
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  
    
